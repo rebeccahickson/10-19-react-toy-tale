2384eb6a82b5e663334df0c3132b180bae0a2b0f
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-04 15:02:59 -0500

    Pod group

--- src/ToyCard.js
+++ src/ToyCard.js
@@ -1,15 +1,31 @@
+import React, { useState, useEffect } from "react";
 
+const ToyCard = (props) => {
+  // const {id, image, name} = props.toy // destructuring is an option
 
-function ToyCard(props){
-    // const {id, image, name} = props.toy // destructuring is an option
-    return(
-        <div class="card" id={`toy-${props.id}`}>
-            <h2>{props.name}</h2>
-            <img src={props.image} class="toy-avatar"/>
-            <p>{props.likes} Likes </p>
-            <button class="like-btn">Like &lt;3</button>
-        </div>
-    )
-}
+  const [timer, setTimer] = useState("");
+  const [seconds, setSeconds] = useState(0);
 
-export default ToyCard
+  useEffect(() => {
+    startTimer();
+    return () => clearInterval(timer);
+  }, []);
+
+  const startTimer = () => {
+    setTimer(setInterval(() => setSeconds((s) => s + 1), 1000));
+  };
+
+  return (
+    <div class="card" id={`toy-${props.id}`}>
+      <h2>{props.name}</h2>
+      <img src={props.image} class="toy-avatar" />
+      <p>{props.likes} Likes </p>
+      <div>{seconds}</div>
+      <button class="like-btn" onClick={props.handleClick}>
+        Like &lt;3
+      </button>
+    </div>
+  );
+};
+
+export default ToyCard;
--- src/ToysContainer.js
+++ src/ToysContainer.js
@@ -1,67 +1,89 @@
-import toysObj from './database'
-import ToyCard from './ToyCard'
-import React from 'react'
+import toysObj from "./database";
+import ToyCard from "./ToyCard";
+import React, { useState, useEffect } from "react";
 
-// when state changes, a rerender is caused 
-// if we want to utilize state we need a Class Component 
+// when state changes, a rerender is caused
+// if we want to utilize state we need a Class Component
 
-// const toys = 
-   
-class ToysContainer extends React.Component{
-    // sets INITIAL STATE
-    state = {
-        toys: [],
-        search: "",
-        whatever: "hello"
+// const toys =
+
+const ToysContainer = () => {
+  // sets INITIAL STATE
+
+  const [toys, setToys] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const url = "http://localhost:3000/toys";
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => {
+        setToys(json);
+      });
+  }, []);
+
+  const handleClick = (event) => {
+    const toyId = event.target.parentElement.id.split("-")[1];
+    const toyIndex = toys.findIndex((toy) => toy.id == toyId);
+    const currentToy = toys[toyIndex];
+    const url = `http://localhost:3000/toys/${toyId}`;
+
+    const configObj = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ likes: currentToy.likes + 1 }),
+    };
+    fetch(url, configObj)
+      .then((res) => res.json())
+      .then((json) => {
+        setToys([
+          ...toys.slice(0, toyIndex),
+          json,
+          ...toys.slice(toyIndex + 1),
+        ]);
+      });
+  };
+
+  const makeToyCards = () => {
+    let displayedToys = toys;
+    if (search) {
+      displayedToys = toys.filter((toy) =>
+        toy.name.toLowerCase().includes(search.toLowerCase())
+      );
     }
 
-    makeToyCards(){
-        //utilize STATE
-        let displayedToys = this.state.toys
-        console.log(this.state.search)
-        if(this.state.search){
-            displayedToys = this.state.toys.filter((toy) =>  
-            toy.name.toLowerCase().includes(this.state.search.toLowerCase()))
-        }
+    return displayedToys.map((toy) => (
+      <ToyCard
+        toy={toy}
+        id={toy.id}
+        name={toy.name}
+        image={toy.image}
+        likes={toy.likes}
+        handleClick={handleClick}
+      />
+    ));
+  };
 
-        return displayedToys.map(toy => <ToyCard toy={toy} id={toy.id} name={toy.name} image={toy.image} likes={toy.likes} />)
-    }
+  const handleInputChange = (e) => {
+    const search = e.target.value;
+    setSearch(search);
+  };
 
-    // componentDidUpdate(){
-    //     console.log("updatesd", this.state)
-    // }
+  return (
+    <div id="toy-container">
+      <div>
+        <input
+          type="text"
+          placeholder="Search for a toy..."
+          onChange={handleInputChange}
+        />
+      </div>
+      {makeToyCards()}
+    </div>
+  );
+};
 
-    componentDidMount(){
-      // where you make your fetch requests 
-      const url ="http://localhost:3000/toys"
-      fetch(url)
-      .then(res => res.json())
-      .then(json => {
-          // deal with the json
-          console.log(json)
-         // this.state.toys = json  //THIS WILL NOT CAUSE A RERENDER
-         this.setState({
-             toys: json
-         })
-      })
-    }
-
-    handleInputChange = (e) => {
-        const search = e.target.value
-        this.setState({search: search}) // will cause a rerender
-    }
-
-
-    render(){
-        return(
-            <div id="toy-container">
-                <div>
-                    <input type="text" placeholder="Search for a toy..." onChange={this.handleInputChange}/>
-                </div>
-               {this.makeToyCards()}
-            </div>
-        ) 
-    }
-}
-
-export default ToysContainer
+export default ToysContainer;
